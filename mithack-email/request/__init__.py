fb2452ab684009e726876c60ae7f4eb174d5b7ef
Jingsi Zhu <zhu@jing.si>
2013-10-06 02:52:31 -0400

    fix

--- mithack-email/request/__init__.py
+++ mithack-email/request/__init__.py
@@ -1,7 +1,26 @@
+import re
+
 from collections import namedtuple
 
-Request = namedtuple('Request', ['command', 'reference'])
+Request = namedtuple('Request', ['command', 'email', 'reference'])
 
 class RequestParser(object):
     def parse(self, email):
-        pass
+        body = email.body.decode()
+
+        splitted = re.split('-{4,}[^-]+-{4,}', body, maxsplit=1)
+        command = None
+        body = None
+        if len(splitted) > 1:
+            command, body = splitted
+        else:
+            command = body = splitted
+
+        ref = re.search('_mew_ref_ (\w+)', body)
+        if ref:
+            ref = ref.group(1)
+
+        return Request(command, body, ref)
+
+
+
