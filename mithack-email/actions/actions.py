a19d484fb14a9488b3a87f73dabd2f9161cd889c
Jingsi Zhu <zhu@jing.si>
2013-10-06 07:18:27 -0400

    parsing pipeline

--- mithack-email/actions/actions.py
+++ mithack-email/actions/actions.py
@@ -1,5 +1,6 @@
 from google.appengine.api.mail import EmailMessage
 import nlp
+import time
 
 class Action(object):
     def __init__(self, context):
@@ -24,15 +25,26 @@ class InvalidAction(Action):
 class NewEventAction(Action):
     def act(self):
         email = self.context.email
-        text = email.body.decode()
+        text = self.context.referenced_email
         subject = email.subject
+
+        subject = subject.replace('Fwd:','').replace('Re:', '').strip()
 
         start_time, end_time = nlp.time_parser.parse(text)
         event_name = ' - '.join(nlp.topic_parser.generate_topics(subject, text))
         loc = nlp.location_parser.parse(text)
 
+        def time_format(t):
+            return time.strftime('%b %d, %Y %H:%M', t)
+
         reply = self.build_reply(self.context.email)
-        reply.body = 'We have created the event for ya.'
+        reply.body = 'We have created the event for you. \n'
+        reply.body += '\n'
+        reply.body += (' * Name: ' + event_name + '\n')
+        reply.body += (' * Start: ' + str(start_time) + '\n')
+        reply.body += (' * End: ' + str(end_time) + '\n')
+        reply.body += (' * Location: ' + (loc) + '\n')
+
         return reply
 
 class NewAppointmentAction(Action):
