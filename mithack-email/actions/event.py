a92f387c926ecb10ce1c12312c0453bc934933d8
Adam Wang <yw287@cornell.edu>
2013-10-06 09:07:42 -0400

    lol

--- mithack-email/actions/event.py
+++ mithack-email/actions/event.py
@@ -3,22 +3,22 @@ from google.appengine.ext import webapp
 from oauth2client.appengine import OAuth2Decorator
 import time
 
-decorator = OAuth2Decorator(
-  client_id='348540417497.apps.googleusercontent.com',
-  client_secret='motfNfjiaxFaEmkBRWKurzXA',
-  scope='https://www.googleapis.com/auth/calendar')
+# decorator = OAuth2Decorator(
+#   client_id='348540417497.apps.googleusercontent.com',
+#   client_secret='motfNfjiaxFaEmkBRWKurzXA',
+#   scope='https://www.googleapis.com/auth/calendar')
 
-service = build('calendar', 'v3')
+# service = build('calendar', 'v3')
 
-class MainHandler(webapp.RequestHandler):
+# class MainHandler(webapp.RequestHandler):
 
-    @decorator.oauth_required
-    def get(self):
-        # Get the authorized Http object created by the decorator.
-        http = decorator.http()
-        # Call the service using the authorized Http object.
-        request = service.events().list(calendarId='primary')
-        response = request.execute(http=http)
+#     @decorator.oauth_required
+#     def get(self):
+#         # Get the authorized Http object created by the decorator.
+#         http = decorator.http()
+#         # Call the service using the authorized Http object.
+#         request = service.events().list(calendarId='primary')
+#         response = request.execute(http=http)
 
 class Event:
     def __init__(self, name, location, start, end):
@@ -34,27 +34,27 @@ class EventAction(object):
     def followup(self, command):
         pass
 
-class AddEvent(webapp2.RequestHandler):
-    @decorator.oauth_aware
-    def post(self):
-        if decorator.has_credentials():
-            event_name = self.request.get('event-name')
-            some_event = {
-                    'summary': self.event.name,
-                    'start': self.start_format,
-                    'end': self.end_format,
-                    'location': self.event.location
-            }
-            http = decorator.http()
-            # Using 'primary' will insert the event for the current user
-            request = service.events().insert(calendarId='primary', body=some_event)
-            inserted = request.execute(http=http)
-            self.response.write(json.dumps(inserted))
-        else:
-            self.response.write(json.dumps({'error': 'No credentials'}))
+# class AddEvent(webapp2.RequestHandler):
+#     @decorator.oauth_aware
+#     def post(self):
+#         if decorator.has_credentials():
+#             event_name = self.request.get('event-name')
+#             some_event = {
+#                     'summary': self.event.name,
+#                     'start': self.start_format,
+#                     'end': self.end_format,
+#                     'location': self.event.location
+#             }
+#             http = decorator.http()
+#             # Using 'primary' will insert the event for the current user
+#             request = service.events().insert(calendarId='primary', body=some_event)
+#             inserted = request.execute(http=http)
+#             self.response.write(json.dumps(inserted))
+#         else:
+#             self.response.write(json.dumps({'error': 'No credentials'}))
 
-    def __init__(self, event):
-        self.event = event
-        self.start_format = time.strftime('%b %d, %Y %H:%M', event.start)
-        self.end_format = time.strftime('%b %d, %Y %H:%M', event.end)
+#     def __init__(self, event):
+#         self.event = event
+#         self.start_format = time.strftime('%b %d, %Y %H:%M', event.start)
+#         self.end_format = time.strftime('%b %d, %Y %H:%M', event.end)
 
